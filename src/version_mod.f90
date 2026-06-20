aab439e216388a95c685d54c1839cb669f63f0da
Sandeep Koranne <sandeep.koranne@gmail.com>
2026-06-19 21:55:57 -0700

    Adding NVFORTRAN support

--- src/version_mod.f90
+++ src/version_mod.f90
@@ -1,4 +1,8 @@
 module version_mod
   implicit none
+<<<<<<< Updated upstream
   character(len=*), parameter :: commit_hash = 'da30647'
+=======
+  character(len=*), parameter :: commit_hash = '350858e'
+>>>>>>> Stashed changes
 end module version_mod
