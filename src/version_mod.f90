196020d9dad944815f569454189982e50d838a0c
Sandeep Koranne <sandeep.koranne@gmail.com>
2026-06-19 22:04:51 -0700

    Makefile fix

--- src/version_mod.f90
+++ src/version_mod.f90
@@ -1,8 +1,4 @@
 module version_mod
   implicit none
-<<<<<<< Updated upstream
-  character(len=*), parameter :: commit_hash = 'da30647'
-=======
-  character(len=*), parameter :: commit_hash = '350858e'
->>>>>>> Stashed changes
+  character(len=*), parameter :: commit_hash = '970e262'
 end module version_mod
