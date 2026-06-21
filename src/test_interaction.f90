fe0bd39c827403fedb10b3b12a3512880b1ae63f
Sandeep Koranne <sandeep.koranne@gmail.com>
2026-06-20 17:30:05 -0700

    Trying to debug 719 CUDA error

--- src/test_interaction.f90
+++ src/test_interaction.f90
@@ -2,18 +2,63 @@
 ! Author : Sandeep Koranne (C) 2026.
 ! Purpose: The RTree for GPU has to be thought from groudup in NVFORTRAN GPU
 !        : as there are several challenges, such as non-allocate, no procs.
-program main
+module Test
   use CommonModule
   use GeometryModule
   use KLDataModule
   use RTreeBuilderGPU
   use MortonSortOMT
   use SystemInformationModule
-  use GPUMergeModule
+  !use GPUMergeModule
   use DatastructuresModule
-  use RLEMergeModule
+  !use RLEMergeModule
   use iso_fortran_env, only: int32, int64, real64
   use omp_lib
+
+contains
+  subroutine PerformMergeGPU(sorted_boxes, num_boxes, root_index, tree_nodes, num_nodes)
+  type(Box), intent(in) :: sorted_boxes(num_boxes)
+  integer(kind=int64), intent(in) :: num_boxes, root_index, num_nodes
+  type(Box), intent(in) :: tree_nodes(num_nodes)   ! pretend a leaf‑only tree
+  integer(kind=int32) :: global_edge_count
+  integer(kind=int32), parameter :: CHUNK_SIZE = 256
+  integer(kind=int32), parameter :: LIMIT_EDGES = CHUNK_SIZE*50
+  integer(kind=int32) :: i, j, idx
+  integer(kind=int32), allocatable :: d_edges(:,:)
+
+  allocate(d_edges(2, LIMIT_EDGES))
+
+  global_edge_count = 0
+  !$omp target data map(to: sorted_boxes(1:num_boxes), &
+  !$omp                tree_nodes(1:num_nodes)) &
+  !$omp               map(tofrom: d_edges) &
+  !$omp               map(tofrom: global_edge_count)
+
+  do i = 1, num_boxes
+     !$omp target teams distribute parallel do &
+     !$omp   private(j, idx) &
+     !$omp   reduction(+:global_edge_count)
+     do j = i+1, num_boxes
+        if (abs(sorted_boxes(i)%X1 - sorted_boxes(j)%X1) < 1.0) then
+           !$omp atomic capture
+           idx = global_edge_count
+           global_edge_count = global_edge_count + 1
+           !$omp end atomic
+           if (idx < LIMIT_EDGES) then
+              d_edges(1, idx+1) = i
+              d_edges(2, idx+1) = j
+           end if
+        end if
+     end do
+  end do
+  !$omp end target data
+
+  print *, "edges generated:", global_edge_count
+end subroutine PerformMergeGPU
+end module Test
+
+program main
+  use Test
   implicit none
 
   type(Box), allocatable :: boxes(:)
@@ -51,19 +96,20 @@ program main
   write(*,*) '|TOTAL INTERACTIONS| = ', interaction_count
   call StopMarkTime("RTree")
   call StartMarkTime("Singleton")
-  call FindSingletonsGPU( boxes, TreeNodes, RootIndex, is_singleton, num_singletons)
+  !call FindSingletonsGPU( boxes, TreeNodes, RootIndex, is_singleton, num_singletons)
   write(*,*) '|NUM_SINGLETONS| = ', num_singletons
   call StopMarkTime("Singleton")
   call StartMarkTime("PNUM")
   overlap_area = 0
   overlap_perimeter = 0 !> if perimeter comes back zero => there was finite overlap, not just touch
-  call PerformMergeGPU(uf, boxes, N, K_LEAF_CAPACITY, TreeNodes, total_nodes, RootIndex, overlap_area, overlap_perimeter)
+  !call PerformMergeGPU(uf, boxes, N, K_LEAF_CAPACITY, TreeNodes, total_nodes, RootIndex, overlap_area, overlap_perimeter)
+  call PerformMergeGPU(boxes, N, RootIndex, boxes, total_nodes)
   write(*,*) 'OVLP AREA = ', overlap_area, ' OVLP PERIMETER = ', overlap_perimeter
   call StopMarkTime("PNUM")
   call StartMarkTime("PNUMRLE")
   overlap_area = 0
   overlap_perimeter = 0  
-  call PerformRLEMergeGPU(uf, boxes, K_LEAF_CAPACITY, TreeNodes, RootIndex, overlap_area, overlap_perimeter)
+  !call PerformRLEMergeGPU(uf, boxes, K_LEAF_CAPACITY, TreeNodes, RootIndex, overlap_area, overlap_perimeter)
   write(*,*) 'OVLP AREA = ', overlap_area, ' OVLP PERIMETER = ', overlap_perimeter  
   call StopMarkTime("PNUMRLW")
 end program main
